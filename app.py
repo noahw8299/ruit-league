3d88ee2eed69bff82afbf792434d070da7fa84e0
Noah Wright <luckydragon1999@gmail.com>
2023-02-12 01:41:25 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,61 +1,60 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
 
-# Set the title of the page
-st.title("Ruit League 2023")
+def main():
+    st.set_page_config(page_title="Ruit League 2023", page_icon=":trophy:", layout="wide")
 
-# Get the names of the two teams and split them by a comma
-team_names = st.text_input("Enter the names of the two teams separated by a comma:").split(',')
+    menu = ["Data Entry", "Data Visualization"]
+    choice = st.sidebar.selectbox("Select an option", menu)
 
-if len(team_names) < 2:
-    st.error("Please enter the names of two teams separated by a comma.")
-else:
-    # Get the final score of the game
-    final_score = st.text_input("Enter the final score (e.g. 1 - 0)")
+    if choice == "Data Entry":
+        st.title("Ruit League Data Entry")
 
-    # Check if there was overtime
-    ot = st.checkbox("Was there overtime?")
+        team_names = st.text_input("Enter comma separated team names:")
+        team_list = team_names.split(',')
 
-    # Get the name and number of cups hit by the first player on team 1
-    player_1_name = st.text_input("Enter the name of the first player for {}".format(team_names[0]), key='player_1_name')
-    player_1_cups = st.text_input("How many cups did {} hit?".format(player_1_name), key='player_1_cups')
+        data = []
+        for team in team_list:
+            team = team.strip()
+            player_1 = st.text_input(f"Enter player name for team {team}:", key=f"{team}_player_1")
+            player1_cups = st.number_input(f"Enter the number of cups hit by {player_1}:", value=0, step=1, key=f"{team}_player1_cups")
+            data.append({'team': team, 'player_name': player_1, 'player_cups': player1_cups})
+            player_2 = st.text_input(f"Enter player name for team {team}:", key=f"{team}_player_2")
+            player2_cups = st.number_input(f"Enter the number of cups hit by {player_2}:", value=0, step=1, key=f"{team}_player2_cups")
+            data.append({'team': team, 'player_name': player_2, 'player_cups': player2_cups})
 
-    # Get the name and number of cups hit by the second player on team 1
-    player_2_name = st.text_input("Enter the name of the second player for {}".format(team_names[0]), key='player_2_name')
-    player_2_cups = st.text_input("How many cups did {} hit?".format(player_2_name), key='player_2_cups')
+        # New additions to track additional data points
+        ot = st.checkbox("Was there overtime?")
+        final_score = st.text_input("Enter the final score (e.g. 1 - 0)")
+        final_cup = st.text_input("Who hit the final cup?")
 
-    # Get the name and number of cups hit by the third player on team 1
-    player_3_name = st.text_input("Enter the name of the first player for {}".format(team_names[1]), key='player_3_name')
-    player_3_cups = st.text_input("How many cups did {} hit?".format(player_3_name), key='player_3_cups')
+        df = pd.DataFrame(data)
 
-    # Get the name and number of cups hit by the fourth player on team 1
-    player_4_name = st.text_input("Enter the name of the second player for {}".format(team_names[1]), key='player_4_name')
-    player_4_cups = st.text_input("How many cups did {} hit?".format(player_4_name), key='player_4_cups')
+        df["overtime"] = ot
+        df["final_cup"] = final_cup
+        df["final_score"] = final_score
+        df["submission_time"] = str(datetime.datetime.now())
 
+        if st.button("Submit"):
+            st.write(df)
 
-    # Get the name of the player who hit the final cup
-    final_cup = st.text_input("Who hit the final cup?")
+            # save the data to a file
+            df.to_csv('ruit_league_data.csv', index=False)
 
-    # Calculate the total number of cups hit by each team
-    team_1_cups = int(player_1_cups) + int(player_2_cups)
-    team_2_cups = int(player_3_cups) + int(player_4_cups)
+            # display a success message
+            st.success("Data submitted successfully!")
+    else:
+        # load the data from the file
+        player_data = pd.read_csv('ruit_league_data.csv')
 
-    # Check if the total number of remaining cups equals 12
-    if int(final_score.split(' - ')[0]) + int(final_score.split(' - ')[1]) != 12:
-        st.error("The total number of remaining cups must add up to 12.")
+        st.write("Player Data:", player_data)
 
-    # Check if the total number of cups hit by team 1 equals 12 minus their final score
-    if team_1_cups != 12 - int(final_score.split(' - ')[0]):
-        st.error("The total number of cups hit by {} players must equal 12 - their final score.".format(team_names[0]))
+        if st.checkbox("Show Plot"):
+            fig, ax = plt.subplots()
+            ax.bar(player_data["player_name"], player_data["player_cups"])
+            st.pyplot(fig)
 
-    if team_2_cups != 12 - int(final_score.split(' - ')[1]):
-        st.error("The total number of cups hit by {} players must equal 12 - their final score.".format(team_names[1]))
-
-    # st.write("The game was played between {} and {}".format(team_names[0], team_names[1]))
-    # st.write("Final Score: {}".format(final_score))
-    # st.write("Overtime: {}".format(ot))
-    # st.write("{} hit {} cups".format(player_1_name, player_1_cups))
-    # st.write("{} hit {} cups".format(player_2_name, player_2_cups))
-    # st.write("{} hit {} cups".format(player_3_name, player_3_cups))
-    # st.write("{} hit {} cups".format(player_4_name, player_4_cups))
-    # st.write("The final cup was hit by {}".format(final_cup))
-
+if __name__ == "__main__":
+    main()
